71ea8ea79c078a904d6f30cc475758a347a2f973
nspies <nspies@stanford.edu>
2017-06-02 11:03:27 -0700

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='genosv',
+      version='0.1',
+      description='genosv',
+      author='Noah Spies',
+      packages=['genosv'],
+     )
