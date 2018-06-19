44f7bfc75bf84c1db4563d9fd30bf20967d1c825
Noah Spies <nspies@stanford.edu>
2018-06-19 12:05:49 -0700

    fixing haplotype splitting when analyzing single-ended reads; fixes #50

--- src/svviz2/io/read_filters.py
+++ src/svviz2/io/read_filters.py
@@ -1,13 +1,21 @@
+def get_haplotype(read_or_pair):
+    try:
+        read = read_or_pair.original_read_ends["1"]
+    except AttributeError:
+        read = read_or_pair
+        
+    cur_hp = None
+    if read.has_tag("HP"):
+        cur_hp = read.get_tag("HP")
+        
+    return cur_hp
+    
 def get_haplotype_filter(hp):
     def filter_haplotype(batch):
         filtered_batch = []
-        for read_pair in batch:
-            cur_hp = None
-            cur_read1 = read_pair.original_read_ends["1"]
-            if cur_read1.has_tag("HP"):
-                cur_hp = cur_read1.get_tag("HP")
-            if cur_hp == hp:
-                filtered_batch.append(read_pair)
+        for read_or_pair in batch:
+            if get_haplotype(read_or_pair) == hp:
+                filtered_batch.append(read_or_pair)
         return filtered_batch
     return filter_haplotype
  
